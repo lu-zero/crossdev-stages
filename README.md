46534bee6323c7aa5207e954ccf6b5decac40148
Luca Barbato <lu_zero@gentoo.org>
2024-07-03 11:23:01 +0200

    feat: Initial stage1 builder script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # crossdev-stages
 Build Gentoo stages leveraging crossdev
+
+## Status
+
+- [x] Build and assemble packages to a stage1 [catalyst](https://wiki.gentoo.org/wiki/Catalyst) can leverage
+- [ ] Update a compatible stage3 image
+- [ ] Assemble bootable images
+
+## Platforms
+- riscv64 (bpi-f3)
+
+## Limitations
+
+- Some packages are cross-compilation unfriendly and rely on runtime checks (e.g. perl modules)
---
+++ mkstage1.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [[ `whoami` != "root" ]]; then
+    echo "This script requires root"
+    exit 1
+fi
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <stage1-directory>"
+    exit 1
+fi
+
+STAGE1_DIR=$1
+
+STAGE1_PACKAGES=`grep -v '#' /var/db/repos/gentoo/profiles/default/linux/packages.build`
+PROFILE=default/linux/riscv/23.0/rv64/split-usr/lp64d
+OUR_CFLAGS="-O3 -march=rv64gcv_zvl256b -pipe"
+OUR_CHOST=riscv64-unknown-linux-gnu
+OUR_KEYWORD=riscv
+CROSSDEV_ROOT=/usr/${OUR_CHOST}
+CROSSDEV_MAKE_CONF=${CROSSDEV_ROOT}/etc/portage/make.conf
+OPTS="-j50 --load-average 100"
+
+
+setup_crossdev() {
+    crossdev riscv64-unknown-linux-gnu
+    PORTAGE_CONFIGROOT=${CROSSDEV_ROOT} eselect profile set ${PROFILE}
+    sed -i -e "s:CFLAGS=.*:CFLAGS=\"${OUR_CFLAGS}\":" ${CROSSDEV_MAKE_CONF}
+}
+
+build_packages() {
+    export EMERGE_DEFAULT_OPTS="$OPTS"
+    export MAKEOPTS="$OPTS"
+    riscv64-unknown-linux-gnu-emerge -b ${STAGE1_PACKAGES}
+}
+
+prepare_stage1() {
+    local root=$1
+
+    mkdir -p ${root}/etc/portage/
+    echo "CHOST=${OUR_CHOST}" > ${root}/etc/portage/make.conf
+    echo "ACCEPT_KEYWORDS=~$OUR_KEYWORD" >> ${root}/etc/portage/make.conf
+    echo "CFLAGS=\"$OUR_CFLAGS\"" >> ${root}/etc/portage/make.conf
+    echo 'CXXFLAGS=$CFLAGS' >> ${root}/etc/portage/make.conf
+    PORTAGE_CONFIGROOT=${root} eselect profile set ${PROFILE}
+}
+
+install_stage1() {
+    export EMERGE_DEFAULT_OPTS="$OPTS"
+    export MAKEOPTS="$OPTS"
+    ROOT=$1 USE=build riscv64-unknown-linux-gnu-emerge -k baselayout
+    ROOT=$1 riscv64-unknown-linux-gnu-emerge -k ${STAGE1_PACKAGES}
+    ROOT=$1 USE=build riscv64-unknown-linux-gnu-emerge portage
+}
+
+setup_crossdev
+build_packages
+prepare_stage1 $STAGE1_DIR
+install_stage1 $STAGE1_DIR
