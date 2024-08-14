fd1830e683f5ada17fa20f32c4ecd7c82502bbc0
Luca Barbato <lu_zero@gentoo.org>
2024-08-14 19:51:57 +0000

    feat: Sync with bianbu-1.0.12

--- make-image.sh
+++ make-image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BOOT_TAG="v1.0.3"
-OPENSBI_REPO="https://github.com/lu-zero/pi-opensbi"
-U_BOOT_REPO="https://github.com/lu-zero/pi-u-boot"
+BOOT_TAG="v1.0.11"
+OPENSBI_REPO="https://gitee.com/bianbu-linux/opensbi.git"
+U_BOOT_REPO="https://gitee.com/bianbu-linux/uboot-2022.10.git"
 FIRMWARE_REPO="https://gitee.com/bianbu-linux/buildroot-ext.git"
-KERNEL_TAG="v1.0.3-lu"
+KERNEL_TAG="v1.0.12-lu"
 KERNEL_REPO="https://github.com/lu-zero/pi-linux"
 
 BASE_DIR=$(dirname $(readlink -f "$0"))
