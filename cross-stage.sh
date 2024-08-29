2d3e52739a98b036d9a54ea806d54d77531962bf
Luca Barbato <lu_zero@gentoo.org>
2024-08-29 10:05:04 +0000

    fix: Update the portage overrides
    The perl ebuild is now already pointing to the right place
    The clang build system expects to be able to run a program for target (to be fixed uptream)
    The compiler-rt* ebuilds would prefer to have a host clang available (to be fixed in crossdev)

--- cross-stage.sh
+++ cross-stage.sh
@@ -50,8 +50,10 @@ setup_crossdev() {
     mkdir ${root}/etc/portage/package.env
     mkdir -p ${root}/etc/portage/package.use
     echo -e '>=virtual/libcrypt-2-r1 static-libs\n>=sys-libs/libxcrypt-4.4.36-r3 static-libs\n>=sys-apps/busybox-1.36.1-r3 -pam static' > ${root}/etc/portage/package.use/busybox
-    echo 'LDFLAGS="$LDFLAGS --sysroot=$EROOT"' > ${root}/etc/portage/env/override-sysroot
-    echo "dev-lang/perl override-sysroot" >${root}/etc/portage/package.env/perl
+    echo -e 'sys-libs/compiler-rt -clang\nsys-libs/compiler-rt-sanitizers -clang' > ${root}/etc/portage/package.use/clang
+    echo "sys-devel/clang -extra" >> ${root}/etc/portage/package.use/clang
+#    echo 'LDFLAGS="$LDFLAGS --sysroot=$EROOT"' > ${root}/etc/portage/env/override-sysroot
+#    echo "dev-lang/perl override-sysroot" >${root}/etc/portage/package.env/perl
     mkdir ${CROSSDEV_ROOT}/bin
     # crossdev starts as split_usr layout
     merge-usr --root ${CROSSDEV_ROOT}
@@ -78,8 +80,8 @@ install_stage1() {
 
 install_perl() {
     local root=${CROSSDEV_ROOT}
-    echo 'LDFLAGS="$LDFLAGS --sysroot=$EROOT"' > ${root}/etc/portage/env/override-sysroot
-    echo "dev-lang/perl override-sysroot" >${root}/etc/portage/package.env/perl
+#    echo 'LDFLAGS="$LDFLAGS --sysroot=$EROOT"' > ${root}/etc/portage/env/override-sysroot
+#    echo "dev-lang/perl override-sysroot" >${root}/etc/portage/package.env/perl
     riscv64-unknown-linux-gnu-emerge perl
     ROOT=$1 riscv64-unknown-linux-gnu-emerge perl
 }
