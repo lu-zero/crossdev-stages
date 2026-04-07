45da2c3b794eae6022215cb3621708b57875bbe7
Sungjoon Moon <sumoon@seoulsaram.org>
2026-04-07 18:53:39 +0900

    fix(boards): fix genimage layouts and board build issues
    - K230: fix SPL offsets to 1024K+1536K, align partitions to 1M
    - KY-X1: fix firmware outside GPT, gpt-location=4M, add clang to sandbox
    - KY-X1: fix board.conf root device and boot console
    - Blackhole: fix board_assemble array expansion
    - All boards: add SYSROOT variable for sysroot isolation

--- boards/ky-x1/board.sh
+++ boards/ky-x1/board.sh
@@ -54,7 +54,7 @@ board_assemble() {
     done
 
     run_with_build_and_source "$sandbox_dir" "$build_dir" "$source_dir" \
-      "${extra_args[@]}" -- "
+      ${extra_args[@]+"${extra_args[@]}"} -- "
         set -e
 
         # Copy DTBs
@@ -76,6 +76,7 @@ board_assemble() {
 
         # Build initramfs
         kver=\$(ls /build/gen/root/lib/modules/ | head -1)
+        [[ -z \"\$kver\" ]] && { echo 'Error: no kernel modules found'; exit 1; }
         dracutbasedir=/usr/lib/dracut \
         DRACUT_INSTALL=/usr/lib/dracut/dracut-install \
           dracut -f --no-early-microcode --no-kernel \
