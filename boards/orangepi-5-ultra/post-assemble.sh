2256f4e6f5910d87ec55334ed726a3f556dd121f
Sungjoon Moon <sumoon@seoulsaram.org>
2026-08-24 06:19:46 +0900

    boards/orangepi-5-ultra: let assemble write the extlinux config

--- boards/orangepi-5-ultra/post-assemble.sh
+++ boards/orangepi-5-ultra/post-assemble.sh
@@ -8,20 +8,6 @@ for dir in "${FIRMWARE_DIRS[@]}"; do
     cp -a "/build/firmware/${dir}/." "/build/gen/root/lib/firmware/${dir}/"
 done
 
-# Extlinux boot config
-mkdir -p /build/gen/boot/extlinux
-kver=$(ls /build/gen/root/lib/modules/ | head -1)
-[ -z "$kver" ] && { echo 'Error: no kernel modules found'; exit 1; }
-cat > /build/gen/boot/extlinux/extlinux.conf << EXTEOF
-DEFAULT gentoo
-TIMEOUT 30
-LABEL gentoo
-    MENU LABEL Gentoo Linux
-    LINUX /${BOOT_KERNEL_NAME}
-    FDT /rk3588-orangepi-5-ultra.dtb
-    APPEND root=${BOOT_ROOT_DEV} rw rootwait rootfstype=ext4 console=${BOOT_CONSOLE} earlycon cma=256M
-EXTEOF
-
 # The stage3 fstab is comments only, so /boot never mounts and a kernel update
 # would land in the rootfs copy instead of the partition u-boot reads.
 cat >> /build/gen/root/etc/fstab <<FSTAB
